0851cfd09c882f9d28a1c089b96f6cd9a8e05a81
product-noob <75776873+product-noob@users.noreply.github.com>
2022-02-20 03:48:27 +0530

    Update scrollbar.js

--- scrollbar.js
+++ scrollbar.js
@@ -5,11 +5,9 @@ let processScroll = () => {
       scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
     scrollPercent = scrollTop / scrollBottom * 100 + '%';
   
-  console.log(scrollTop + ' / ' + scrollBottom + ' / ' + scrollPercent);
+ //console.log(scrollTop + ' / ' + scrollBottom + ' / ' + scrollPercent);
   
     document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent); 
 }
 
 document.addEventListener('scroll', processScroll);
-
-//https://www.youtube.com/watch?v=uvU_Bv3XDGc
